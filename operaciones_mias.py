6ac7c0aa0f94eeea523576d71858eefbb6485cd8
Esteban Daniel Mora Bórquez <esteban.mora2020@twk.cl>
2020-06-10 16:52:29 -0400

    Ejercicio operaciones aritméticas

---
+++ operaciones_mias.py
@@ -0,0 +1,11 @@
+print("operaciones aritméticas")
+num1 = int(input("introduce el primer número: "))
+num2 = int(input("introduce el segundo número: "))
+
+print("La suma de tus números es: ", num1 + num2)
+print("La resta de tus números es: ", num1 - num2)
+print("La multiplicación de tus números es: ", num1 * num2)
+print("La división de tus números es: ", num1 / num2)
+print("El módulo de tus números es: ", num1 % num2)
+print("El entero de tus números es: ", num1 // num2)
+print("El elevado de tus números es: ", num1 ** num2)
